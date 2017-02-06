2530cd10e5e93effbd51e554d23c2490c915e8cb
Vikram Waradpande <f2015454@pilani.bits-pilani.ac.in>
2017-02-06 20:44:27 +0530

    Add files via upload

---
+++ sliderscript.js
@@ -0,0 +1,13 @@
+$('.button').click(function(){
+  
+  $(this).parents('.belt').toggleClass('transformed');
+});
+
+
+$('.button').hover(function(){
+  $(this).toggleClass('button-press')
+})
+
+$('.slide2 .button').click(function(){
+  
+})
